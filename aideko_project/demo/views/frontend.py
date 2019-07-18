31398f0f4f0724370b652e83bba256609698b763
Anton Vlasov <anton.vlasov@jetstyle.ru>
2019-07-18 15:25:26 +0500

    raw views

--- aideko_project/demo/views/frontend.py
+++ aideko_project/demo/views/frontend.py
@@ -9,9 +9,13 @@ APP_DIR: str = '/'.join(CUR_DIT_DECONSTRUCTED[0:len(CUR_DIT_DECONSTRUCTED)-2]) +
 NEWS_PATH: str = os.path.join(APP_DIR, 'news.json')
 COMMENTS_PATH = os.path.join(APP_DIR, 'comments.json')
 
+
+def get_model_data(path):
+    with open(path, 'r') as f:
+        return json.loads(f.read())    
+
 async def news_list(request: 'Request') -> 'Response':
-    with open(NEWS_PATH, 'r') as f:
-        news_dict: dict = json.loads(f.read())
+    news_dict: dict = get_model_data(NEWS_PATH)
 
     news_list: list = []
 
@@ -26,6 +30,24 @@ async def news_list(request: 'Request') -> 'Response':
     return Response(text=json.dumps(news_dict, indent=4))
 
 
-async def news_detail(request):
-    print(request)
-    return Response(text='Ok')
+async def news_detail(request: 'Request') -> 'Response':
+    try:
+        news_item_id = int(str(request.rel_url)[1:])
+        news_dict: dict = get_model_data(NEWS_PATH)
+
+        if news_item_id not in set([news['id'] for news in news_dict['news']]):
+            raise ValueError
+
+    except ValueError:
+        return Response(text='{\n\t"error": "news id is incorrect"\n}')
+
+    comments_dict: dict = get_model_data(COMMENTS_PATH)
+
+    news_item: dict = news_dict['news'][news_item_id-1]
+    news_item['comments'] = []
+
+    for comment in comments_dict['comments']:
+        if comment['news_id'] == news_item_id:
+            news_item['comments'].append(comment)
+
+    return Response(text=json.dumps(news_item, indent=4))
